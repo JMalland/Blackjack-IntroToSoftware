7deeee9461b5f4b7c382df08a7711f7145d75d08
Jacob Malland <mallandj@uwplatt.edu>
2024-10-18 15:58:33 -0500

    Created a Card ScriptableObject, so cards can be generated via code.

---
+++ Assets/CardDisplay.cs
@@ -0,0 +1,82 @@
+using System;
+using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+public class CardDisplay : MonoBehaviour
+{
+    // The card Suit & Type
+    public TextMeshProUGUI _cardSuit;
+    public TextMeshProUGUI _cardType;
+
+    // The front & back images
+    public SpriteRenderer _frontCardImage;
+    //public Image _backCardImage;
+
+    // Array of the Card ScriptableObjects
+    public CardModelSO[] cards;
+
+    // Each Card has an index that will be used to load the card properties
+    void LoadSelectedCard(int index) {
+        Debug.Log("Card Suit: " + cards[index].cardSuit);
+        Debug.Log("Card Type: " + cards[index].cardType);
+
+        // Check if _cardSuit exists
+        if (_cardSuit == null) {
+            Debug.LogError("_cardSuit is not assigned in " + gameObject.name);
+        }
+        // The child variable "_cardSuit" exists
+        else {
+            _cardSuit.text = cards[index].cardSuit;
+        }
+
+        // Check if _cardType exists
+        if (_cardType == null) {
+            Debug.LogError("_cardType is not assigned in " + gameObject.name);
+        }
+        // The child variable "_cardType" exists
+        else {
+            _cardType.text = cards[index].cardType;
+        }
+        
+        // Check if _frontCardImage exists
+        if (_frontCardImage == null) {
+            Debug.LogError("_frontCardImage is not assigned in " + gameObject.name);
+        }
+        // The child variable "_frontCardImage" exists
+        else {
+            _frontCardImage.GetComponent<SpriteRenderer>().sprite = cards[index].frontCardImage;
+            //_frontCardImage = Instantiate(cards[index].frontCardImage, transform.position, Quaternion.identity);
+        }
+        
+        //_backCardImage.sprite = cards[index].frontCardImage;
+    }
+
+    // Reset is called every time a component is added, or reset. This way changes appear in the editor.
+    void Reset()
+    {
+        _cardSuit = GameObject.Find("_cardSuit").GetComponent<TextMeshProUGUI>();
+        _cardType = GameObject.Find("_cardType").GetComponent<TextMeshProUGUI>();
+        _frontCardImage = GameObject.Find("_frontCardImage").GetComponent<SpriteRenderer>();
+        //transform.Find("_frontCardImage").gameObject;
+
+        // Load all of the CardModel objects
+        this.cards = Resources.LoadAll<CardModelSO>("ScriptableObjects/PlayingCards");
+        
+        // Display a random card
+        LoadSelectedCard(UnityEngine.Random.Range(0, cards.Length));
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        // Display a random card
+        LoadSelectedCard(UnityEngine.Random.Range(0, cards.Length));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/CardModelSO.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardModelSO", menuName = "New Playing Card")]
+public class CardModelSO : ScriptableObject
+{
+    // The suit of the card
+    public string cardSuit;
+    
+    // The string value of the card (2,3,4 ... A, J, Q, K)
+    public string cardType;
+    
+    // The card image
+    public Sprite frontCardImage;
+
+    // The back card image
+    public Sprite backCardImage;
+}
