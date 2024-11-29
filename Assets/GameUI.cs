2cc088a34ab2b7300e6812c15b88bffbd6a00eca
ethan <pulvermachee@uwplatt.edu>
2024-11-28 23:01:09 -0600

    added "SetPlayerBet" fxn, still needs to be implemented

--- Assets/GameUI.cs
+++ Assets/GameUI.cs
@@ -1,12 +1,22 @@
-
+using System.Collections;
+using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameUI : MonoBehaviour
 {
-    public void GetPlayerBet(int playerBet)
-    {
+    private int bet;
+    public TMP_InputField BetInput;
 
+    public void SetPlayerBet()
+    {
+        string stringBet = BetInput.text;
+        int bet = Int32.Parse(stringBet);
+        this.bet = bet;
     }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
